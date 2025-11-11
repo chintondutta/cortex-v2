97fd82ed064f3f9a7d6b60460e4b06f970ecddff
chintondutta <chintondutta.tech@gmail.com>
2025-11-11 14:21:34 +0530

    05: ai-jobs

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,10 +1,18 @@
+import { openai, createAgent } from "@inngest/agent-kit";
 import { inngest } from "./client";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
-  async ({ event, step }) => {
-    await step.sleep("wait-a-moment", "1s");
-    return { message: `Hello ${event.data.email}!` };
-  },
+  async ({ event }) => {
+      const codeAgent = createAgent({
+        name: "code-agent",
+        system: "You are a next.js developer. You write readable and maintainable code. You write Next.js & React snippets.",
+        model: openai({ model: "gpt-3.5-turbo", apiKey: process.env.OPENAI_API_KEY }),
+      });
+      const {output} = await codeAgent.run(
+        `Write the following snippet: ${event.data.value}`,
+      );
+      return { output };
+    },
 );
