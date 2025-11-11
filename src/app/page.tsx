9ac5ecb2dd2192bd7d23fa83a8a2c898b3905d4b
chintondutta <chintondutta.tech@gmail.com>
2025-11-11 11:23:56 +0530

    04: background jobs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,18 @@
-// 'use client';
-// import {useTRPC} from '@/trpc/client';
-// import { useQuery } from '@tanstack/react-query';
-// import { caller } from '@/trpc/server';
-import {getQueryClient, trpc} from '@/trpc/server';
-import {dehydrate, HydrationBoundary} from '@tanstack/react-query';
-import { Suspense } from 'react';
-import Client from './client';
+"use client";
 
-const Page = async () => {
-// const trpc = useTRPC();
-// const {data} = useQuery(trpc.hello.queryOptions({text: 'world'}));
-// const data = await caller.hello({text: 'world'});
-const queryClient = getQueryClient();
-void queryClient.prefetchQuery(trpc.hello.queryOptions({text: 'world'}));
+import { Button } from "@/components/ui/button";
+import { useTRPC } from "@/trpc/client";
+import { useMutation } from "@tanstack/react-query";
+
+const Page = () => {
+  const trpc = useTRPC();
+  const invoke = useMutation(trpc.invoke.mutationOptions({}));
   return (
-    <HydrationBoundary state={dehydrate(queryClient)}>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Client />
-      </Suspense>
-    </HydrationBoundary>
+    <div className="p-4 max-w-7xl mx-auto"> 
+      <Button onClick={() => invoke.mutate({text: 'world'})}>
+        Invoke Background Job
+      </Button>
+    </div>
   );
 }
 
