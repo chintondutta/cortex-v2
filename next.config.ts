6bcc40b1e58e619f8fb5dba6b5d55cf9b26473ed
chintondutta <chintondutta.tech@gmail.com>
2025-11-18 12:04:41 +0530

    21: deployment

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,14 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "standalone",
-  outputFileTracingIncludes: {
-    "/*": ["./src/generated/prisma/**/*"],
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals = config.externals || [];
+      config.externals.push({
+        '@prisma/client': '@prisma/client',
+      });
+    }
+    return config;
   },
 };
 
